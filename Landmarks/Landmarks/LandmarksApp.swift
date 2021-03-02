5970e3a9a8d239ab10e41e167b48c0269f56ccad
taevel02 <taevel02@gmail.com>
2021-03-02 18:44:04 +0900

    success creating and combining views to swiftui tutorials

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Taehoon Kwon on 2021/03/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
