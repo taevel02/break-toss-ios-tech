f49cd40a5d67efede8ba0165db6863751c656373
taevel02 <taevel02@gmail.com>
2021-03-05 12:59:15 +0900

    success handling user input to swiftui tutorials

---
+++ Landmarks/Landmarks/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  Landmarks
+//
+//  Created by Taehoon Kwon on 2021/03/05.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    
+    var body: some View {
+        Button(action: {
+            isSet.toggle()
+        }) {
+            Image(systemName: isSet ? "star.fill" : "star")
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+            .previewLayout(.fixed(width: 100, height: 100))
+    }
+}
